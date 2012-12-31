679f7ede17ac37451e1ec9723afeb0d369278235
Dominic <dom@domandtom.com>
2012-12-31 03:35:16 -0500

    update plugins.js to match bootstrap 4.0.2

--- resources/js/plugins.js
+++ resources/js/plugins.js
@@ -1,12 +1,18 @@
 // Avoid `console` errors in browsers that lack a console.
 if (!(window.console && console.log)) {
 (function() {
+        var method;
         var noop = function() {};
         var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
         var length = methods.length;
-        var console = window.console = {};
+        var console = (window.console = window.console || {});
         while (length--) {
-            console[methods[length]] = noop;
+            method = methods[length];
+
+            // Only stub undefined methods.
+            if (!console[method]) {
+                console[method] = noop;
+            }
         }
     }());
 }
