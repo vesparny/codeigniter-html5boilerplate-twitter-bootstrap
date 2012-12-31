48f095ffbb924d1971ffca3c4d8b03296b1f05f5
Dominic <dom@domandtom.com>
2012-12-31 03:31:32 -0500

    update jquery to 1.8.3

--- application/views/template/skeleton.php
+++ application/views/template/skeleton.php
@@ -39,8 +39,8 @@
 <body>
 	<?php echo $body ?>
 	<script
-		src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo base_url(JS."libs/jquery-1.8.2.min.js");?>"><\/script>')</script>
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo base_url(JS."libs/jquery-1.8.3.min.js");?>"><\/script>')</script>
 	<script src="<?php echo base_url(JS."libs/underscore-1.3.3.min.js");?>"></script>
 	<script src="<?php echo base_url(JS."plugins.js");?>"></script>
 	<script src="<?php echo base_url(JS."script.js");?>"></script>
