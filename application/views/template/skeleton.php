6b0b1d194d3b81e4303aa0321233a7f43bf9b518
P R Dinesh <pr.dinesh@live.in>
2013-03-02 09:42:37 +0530

    Updated underscore to 1.4.4 and selected jquery 1.9.1 as default

--- application/views/template/skeleton.php
+++ application/views/template/skeleton.php
@@ -39,9 +39,9 @@
 <body>
 	<?php echo $body ?>
 	<script
-		src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo base_url(JS."libs/jquery-1.8.3.min.js");?>"><\/script>')</script>
-	<script src="<?php echo base_url(JS."libs/underscore-1.4.3.min.js");?>"></script>
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo base_url(JS."libs/jquery-1.9.1.min.js");?>"><\/script>')</script>
+	<script src="<?php echo base_url(JS."libs/underscore-1.4.4.min.js");?>"></script>
 	<script src="<?php echo base_url(JS."plugins.js");?>"></script>
 	<script src="<?php echo base_url(JS."script.js");?>"></script>
 
