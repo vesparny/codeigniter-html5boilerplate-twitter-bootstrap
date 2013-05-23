c67759a31c640655c8da2194af926fdffa0d3fee
navblue <agolfer@gmail.com>
2013-05-23 12:32:28 -0600

    Update basejs.php
    only set uri_segment_x if isset $uri_segmentx

--- application/views/template/basejs.php
+++ application/views/template/basejs.php
@@ -4,8 +4,8 @@ var ARNY = (function(){
 	return{
 		"language": "<?php echo $this->config->item('language'); ?>",
 		"baseUrl": _baseUrl,
-		"uri_segment_1":"<?php echo $uri_segment_1;?>",
-		"uri_segment_2":"<?php echo $uri_segment_2;?>"
+		"uri_segment_1":"<?php echo (isset($uri_segment1)) ? $uri_segment_1 : '';?>",
+		"uri_segment_2":"<?php echo (isset($uri_segment2)) ? $uri_segment_2 : '';?>"
 	}
 })();
 </script>
