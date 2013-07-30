7555d169fa73ac7dc6ad2f979010415010f312a1
Ben Warburton <ben@travoltron.com>
2013-07-30 12:20:09 -0500

    Tweaked the nav helper to match new Bootstrap Styling

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,4 +1,4 @@
-<ul class="nav" >
+<ul class="nav navbar-nav">
 	<li class="<?php echo isActive($pageName,"home")?>"><a href="<?php echo  base_url()?>">Home</a></li>
 	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
 	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
